78ddd8e48b23cd360407e5a513454fa36d80d973
Rick Aqp <richartescobedo@gmail.com>
2020-11-21 08:50:44 -0500

    Add files via upload

---
+++ ajax.php
@@ -0,0 +1,20 @@
+<?php
+
+$team = array(
+	1=>'José',
+	2=>'María',
+	3=>'Jesus',
+	4=>'Estefany',
+	5=>'Carlos',
+	6=>'Teresa',
+	7=>'Luis',
+	8=>'Juan',
+	9=>'Sofia',
+	10=>'Marco'
+);
+
+$number = rand(1,10);
+
+echo $team[$number];
+
+?>
